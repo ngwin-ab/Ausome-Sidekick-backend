f68dac8b4f98ee8b451a9a5ac33cd60119846c4c
An Ba <ngwin.ab@gmail.com>
2023-07-30 11:08:41 -0400

    First commit

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const app = express();
+const bodyParser = require('body-parser');
+const port = 3000;
+
+//middleware
+app.use(bodyParser.json())
+
+dotenv.config();
+
+app.get('/kids', (req, res) => {
+    const kid = {
+        id: 1,
+        name: 'weee',
+        image: 'some_url',
+    }
+    res.send(kid);
+})
+
+app.post('/kids', (req, res) => {
+    const newKid = req.body;
+    console.log(newKid)
+    res.send(newKid);
+})
+
+app.listen(port, ()=>{
+    console.log(`server is running on ${port}`);
+})
