671bcfda6777d154b8f7090dbd2bc3b1a4e6cd6c
An Ba <ngwin.ab@gmail.com>
2023-08-01 10:36:58 -0400

    finish CRUD routes for chart

--- routes/chartsRoute.js
+++ routes/chartsRoute.js
@@ -1,23 +1,13 @@
 const express = require('express');
-const router = express.Router(); 
+const router = express.Router();
 const {Chart} = require('../models/Chart');
+
 // const timestamp = moment(date).format('MM-DD-YYYY');
 
-// =================== GET ALL CHARTS ========================
-router.get('/', async (req, res) => {
-    const chartList = await Chart.find();
+// =================== CREATE ========================
 
-    if(!chartList) {
-        res.status(500).json({
-            success: false
-        })
-    }
-    res.send(chartList);
-})
-
-// =================== POST NEW CHART ========================
-router.post('/', (req, res) => {
-    const chart = new Chart ({
+router.post('/', async (req, res) => {
+    let chart = new Chart({
         timestamp: req.body.timestamp,
         setting: req.body.setting,
         antecedent: req.body.antecedent,
@@ -25,14 +15,82 @@ router.post('/', (req, res) => {
         consequence: req.body.consequence
     })
 
-    chart.save().then((createdChart=> {
-        res.status(201).json(createdChart)
-    })).catch((err)=> {
-        res.status(500).json({
-            error: err,
-            success: false
-        })
-    })
+    chart = await chart.save();
+
+    if (!chart) {
+        return res.status(400).send('The chart could not be created!')
+    }
+
+    res.send(chart);
+})
+
+// =================== READ ========================
+
+router.get('/', async (req, res) => {
+    const chartList = await Chart.find();
+
+    if (!chartList) {
+        res.status(400).json({ success: false })
+    }
+
+    res.status(200).send(chartList);
+})
+
+router.get('/:id', async (req, res) => {
+    const chart = await Chart.findById(req.params.id);
+
+    if (!chart) {
+        res.status(404).json({ success: false, message: "Chart not found!" })
+    }
+
+    res.status(200).send(chart)
+})
+
+// =================== UPDATE  ========================
+
+router.put('/:id', async (req, res) => {
+    Chart.findByIdAndUpdate(req.params.id, {
+                    timestamp: req.body.timestamp,
+                    setting: req.body.setting,
+                    antecedent: req.body.antecedent,
+                    behavior: req.body.behavior,
+                    consequence: req.body.consequence,
+                }, { new: true} ).then(chart => {
+        if (chart) {
+            return res.status(200).send(chart)
+        } else { return res.status(404).json({ success: false, message: "Chart not found!" }) }
+    }).catch(err => (err => {
+        return res.status(400).json({ success: false, error: err })
+    }))
+})
+// router.put('/:id', async (res, req) => {
+//     const chart = await Chart.findByIdAndUpdate(req.params.id,
+//         {
+//             timestamp: req.body.timestamp,
+//             setting: req.body.setting,
+//             antecedent: req.body.antecedent,
+//             behavior: req.body.behavior,
+//             consequence: req.body.consequence,
+//         },
+//         { new: true})
+
+//     if (!chart) {
+//         res.status(404).json({ success: false, message: "Chart not found!" })
+//     }
+
+//     res.status(200).send(chart)
+// })
+
+// =================== DELETE ========================
+
+router.delete('/:id', (req, res) => {
+    Chart.findByIdAndRemove(req.params.id).then(chart => {
+        if (chart) {
+            return res.status(200).json({ success: true, message: "The chart has been successfully deleted!" })
+        } else { return res.status(404).json({ success: false, message: "Chart not found!" }) }
+    }).catch(err => (err => {
+        return res.status(400).json({ success: false, error: err })
+    }))
 })
 
 module.exports = router;
