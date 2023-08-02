2ea3358b1b708cee45e7b0060ea2aef63b12a8fa
An Ba <ngwin.ab@gmail.com>
2023-08-02 01:09:52 -0400

    establish one-many relationship btw Kid and Chart

--- models/Chart.js
+++ models/Chart.js
@@ -2,11 +2,16 @@ const mongoose = require('mongoose');
 const moment = require('moment');
 
 const ChartSchema = mongoose.Schema({
-    timestamp: {type: String, required: true},
-    setting: {type: String, required: true},
-    antecedent: {type: String, required: true},
-    behavior: {type: String, required: true},
-    consequence: {type: String, required: true},
+    timestamp: { type: String, required: true },
+    setting: { type: String, required: true },
+    antecedent: { type: String, required: true },
+    behavior: { type: String, required: true },
+    consequence: { type: String, required: true },
+    kid_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Kid',
+        required: true
+    }
 });
 
 // const timestamp = moment(date).format('MM-DD-YYYY');
--- models/Kid.js
+++ models/Kid.js
@@ -1,9 +1,17 @@
 const mongoose = require('mongoose');
 
 const KidSchema = mongoose.Schema({
-    name: {type: String, required: true},
-    imgPath: {type: String, required: true},
-    charts: { type : Array , default : [] }
+    name: { type: String, required: true },
+    imgPath: { type: String, required: true }
 });
+
+KidSchema.virtual('chartsRecorded', {
+    ref: 'Chart',
+    localField: '_id',
+    foreignField: 'kid_id'
+});
+
+KidSchema.set('toObject', { virtuals: true });
+KidSchema.set('toJSON', { virtuals: true });
 
 exports.Kid = mongoose.model('Kid', KidSchema);
