207089bf5e7837db8df43053652210d0d4c3dad6
An Ba <ngwin.ab@gmail.com>
2023-08-14 19:00:16 -0400

    change port

--- app.js
+++ app.js
@@ -7,11 +7,10 @@ const cors = require('cors');
 const kidsRoute=require('./routes/kidsRoute');
 const chartsRoute = require('./routes/chartsRoute');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 80;
 
 //connect app with mongo database through connection string in .env file
 dotenv.config();
-// mongoose.connect(process.env.MONGODB_URI||'mongodb://127.0.0.1:27017/ausome_db')
 mongoose.connect(process.env.MONGODB_URI)
 .then(()=>{
     app.use(cors());
@@ -24,7 +23,6 @@ mongoose.connect(process.env.MONGODB_URI)
     //routes
     app.use('/kids', kidsRoute);
     app.use('/charts', chartsRoute);
-    
     console.log('Database connected')
     app.listen(port, ()=>{
         console.log(`Server is running on ${port}`);
